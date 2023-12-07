e36b500153d11c5fbc85a0178fc1facb60d18d0e
Dantyputri <138180791+Dantyputri@users.noreply.github.com>
2023-12-07 13:28:50 +0700

    Add files via upload

---
+++ numerik.py
@@ -0,0 +1,19 @@
+ # Contoh kode Python dengan operasi numerik dan fungsi matematika tambahan
+
+# Eksponensiasi (pangkat)
+angka1 = 2
+angka2 = 3
+hasil_pangkat = angka1 ** angka2
+print("Hasil Pangkat:", hasil_pangkat)
+
+# Pembagian sisa
+angka3 = 10
+angka4 = 3
+hasil_sisa_pembagian = angka3 % angka4
+print("Sisa Pembagian:", hasil_sisa_pembagian)
+
+# Operasi campuran
+angka5 = 5
+angka6 = 2
+hasil_campuran = (angka5 + angka6) * angka1 - angka2 / angka6
+print("Hasil Operasi Campuran:", hasil_campuran)
